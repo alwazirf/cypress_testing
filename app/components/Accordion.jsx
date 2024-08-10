c83eacd41f7459ade8218a8b001ce4b174d604ea
alwazirf <alwazirf@gmail.com>
2024-08-11 01:20:09 +0700

    feat: Component testing using cypress

--- app/components/Accordion.jsx
+++ app/components/Accordion.jsx
@@ -1,14 +1,17 @@
-import * as React from "react";
-import Accordion from "@mui/material/Accordion";
-import AccordionSummary from "@mui/material/AccordionSummary";
-import AccordionDetails from "@mui/material/AccordionDetails";
-import Typography from "@mui/material/Typography";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import * as React from 'react';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 export default function ItemsAccordion({ items }) {
   return (
-    <div style={{ maxWidth: "70vw", minWidth: "50vw" }}>
-      {items.map((item) => {
+    <div
+      data-test="accordion-wrapper"
+      style={{ maxWidth: '70vw', minWidth: '50vw' }}
+    >
+      {items?.map((item) => {
         return (
           <Accordion data-test={`accordion-item-${item.id}`} key={item.id}>
             <AccordionSummary
